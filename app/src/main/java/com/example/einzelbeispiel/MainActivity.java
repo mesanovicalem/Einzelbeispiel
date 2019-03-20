3e2b960ac3a28cfc8d7bae36191a8216805acd32
mesanovicalem <alemmesanovic@outlook.com>
2019-03-20 14:32:40 +0100

    TCP Client bekommt die Werte aus der App.
    Network access in Manifest angepasst.
    
    Kommentare wurden zu der Hauptaufgabe hinzugefügt.

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -17,6 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button abschicken = (Button) findViewById(R.id.button1);
+        abschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText eingabeFeld = (EditText) findViewById(R.id.editText);
+                TextView serverAntwort = (TextView) findViewById(R.id.textView2);
+
+                String value = eingabeFeld.getText().toString();
+
+                TCPClient client = new TCPClient(value);
+                Thread thread = new Thread(client);
+
+                thread.start();
+
+
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                serverAntwort.setText(client.modifiedSentence);
+            }
+        });
+
         Button berechnen = (Button) findViewById(R.id.button2);
         berechnen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,10 +53,13 @@ public class MainActivity extends AppCompatActivity {
                 String value = eingabeFeld.getText().toString();
 
                 int[] ziffern = new int[value.length()];
+
+                //Jede Ziffer wird einer Position im int Array zugeordnet.
                 for (int i = 0; i < value.length(); i++) {
                     ziffern[i] = Integer.parseInt(String.valueOf(value.charAt(i)));
                 }
 
+                //Überprüft ob es sich bei der Ziffer um eine Primzahl handelt.
                 for (int j = 0; j < value.length(); j++) {
                     if (ziffern[j] % 2 == 0 || ziffern[j] % 3 == 0) {
                         continue;
@@ -40,9 +68,11 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                Arrays.sort(ziffern);
+                Arrays.sort(ziffern); //Array wird aufsteigend sortiert.
 
                 int count = 0;
+
+                //Hier wurden die Nullen gezählt
                 for (int n = 0; n < value.length(); n++) {
                     if (ziffern[n] == 0) {
                         count++;
@@ -51,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
 
                 int[] arr = new int[value.length() - count];
                 int x = 0;
+
+                //Ziffern werden ins Hilfsarray übertragen, Nullen werden ausgelassen.
                 for (int k = 0; k < value.length(); k++) {
                     if (ziffern[k] == 0) {
                         continue;
@@ -60,11 +92,11 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                Arrays.sort(arr);
+                Arrays.sort(arr); // Das Hilfsarray wird aufsteigend sortiert.
 
-                String endErgebnis = Arrays.toString(arr);
+                String endErgebnis = Arrays.toString(arr); // Hilfsarray wird zu einem String umgewandelt.
 
-                ergebnis.setText(endErgebnis);
+                ergebnis.setText(endErgebnis); //setText
 
                 String message = "Die Ziffern wurden nach der Größe sortiert, die Primzahlen wurden gestrichen.";
 
--- app/src/main/java/com/example/einzelbeispiel/TCPClient.java
+++ app/src/main/java/com/example/einzelbeispiel/TCPClient.java
@@ -1,4 +1,59 @@
 package com.example.einzelbeispiel;
 
-public class TCPClient {
+import java.io.*;
+import java.net.*;
+
+public class TCPClient implements Runnable {
+
+    String sentence;
+    String modifiedSentence;
+
+    TCPClient(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public void run() {
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("se2-isys.aau.at", 53212);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DataOutputStream outToServer = null;
+        try {
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader inFromServer = null;
+        try {
+            inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            outToServer.writeBytes(sentence + '\n');
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            modifiedSentence = inFromServer.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("FROM SERVER:" + modifiedSentence);
+
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
