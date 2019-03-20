1211c8ce9af6d9ce7595bde866b9093b95481728
mesanovicalem <alemmesanovic@outlook.com>
2019-03-20 20:10:52 +0100

    Final Commit

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
                 thread.start();
 
-
                 try {
                     thread.join();
                 } catch (InterruptedException e) {
